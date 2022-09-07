affbf294a986091101592485d6f889cda268ece3
Bharath-SR <bharath.s0941@gmail.com>
2022-09-07 09:46:27 +0530

    Added:Calculate Daily Employee Wage

--- src/com/bridgeLabz/EmployeeWage.java
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -15,20 +15,22 @@ public class EmployeeWage {
 	 * @param args
 	 */
 		// TODO Auto-generated method stub
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
 
-			Random random = new Random();
-			int attendance = random.nextInt(3); 
-			
-			if(attendance==1) {
-				System.out.println("Employee is Present");
-				
-			}
-			else {
-				System.out.println("Employee is absent");
-			}
+		Random random = new Random();
+		int attendance = random.nextInt(2); 
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int wage = wagePerHour*fullDayHour;
+		if(attendance==1) {
+			System.out.println("Employee is Present");
+			System.out.println("Employee Wage is:"+wage);
+		}
+		else {
+			System.out.println("Employee is absent");
+		}
 	}
 
 }
