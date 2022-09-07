2580e36e486acc82dd9931f6c5f606876668d4c4
Bharath-SR <bharath.s0941@gmail.com>
2022-09-07 09:39:20 +0530

    Started Employee Wage Program

---
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgeLabz;
+
+/**
+ * @author Admin
+ *
+ */
+public class EmployeeWage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
