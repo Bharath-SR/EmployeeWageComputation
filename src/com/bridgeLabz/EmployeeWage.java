e89a16ed148252554a8c535b68ea94dbade5484f
Bharath-SR <bharath.s0941@gmail.com>
2022-09-07 09:48:12 +0530

    Added: Employee Wage Part Time

--- src/com/bridgeLabz/EmployeeWage.java
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -14,19 +14,24 @@ public class EmployeeWage {
 	/**
 	 * @param args
 	 */
-		// TODO Auto-generated method stub
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 
 		Random random = new Random();
-		int attendance = random.nextInt(2); 
+		int attendance = random.nextInt(3); 
 		int wagePerHour = 20;
 		int fullDayHour = 8;
+		int partTime =4;
 		int wage = wagePerHour*fullDayHour;
+		int partTimeWage= wagePerHour*partTime;
 		if(attendance==1) {
 			System.out.println("Employee is Present");
 			System.out.println("Employee Wage is:"+wage);
+		}
+		else if(attendance==2) {
+			System.out.println("Employee is Present Part Time");
+			System.out.println("Part Time  Employee Wage is:"+partTimeWage);
 		}
 		else {
 			System.out.println("Employee is absent");
