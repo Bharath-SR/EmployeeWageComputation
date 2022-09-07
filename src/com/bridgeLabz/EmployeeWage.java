8cf91e6c4b6b43880489d67d08097a3f83fa59b2
Bharath-SR <bharath.s0941@gmail.com>
2022-09-07 09:43:00 +0530

    Added: Check Employee is Present or Absent

--- src/com/bridgeLabz/EmployeeWage.java
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -3,8 +3,10 @@
  */
 package com.bridgeLabz;
 
+import java.util.Random;
+
 /**
- * @author Admin
+ * @author Bharath
  *
  */
 public class EmployeeWage {
@@ -12,9 +14,21 @@ public class EmployeeWage {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
 
+			Random random = new Random();
+			int attendance = random.nextInt(3); 
+			
+			if(attendance==1) {
+				System.out.println("Employee is Present");
+				
+			}
+			else {
+				System.out.println("Employee is absent");
+			}
 	}
 
 }
