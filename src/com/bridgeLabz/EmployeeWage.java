fd5d69499ec8dbd4e2c11ebf61ff3cf50b7905eb
Bharath-SR <bharath.s0941@gmail.com>
2022-09-10 09:23:01 +0530

    Added Employee Wage Per Month

--- src/com/bridgeLabz/EmployeeWage.java
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -22,19 +22,20 @@ public class EmployeeWage {
 		int attendance = random.nextInt(3); 
 		int wagePerHour = 20;
 		int fullDayHour = 8;
+		int perMonthDays = 20;
 		int partTime =4;
-		int wage = wagePerHour*fullDayHour;
-		int partTimeWage= wagePerHour*partTime;
+		int wage = wagePerHour*fullDayHour*perMonthDays;
+		int partTimeWage= wagePerHour*partTime*perMonthDays;
 		
 
 		switch(attendance) {
 		case 1:
 			System.out.println("Employee is Present");
-			System.out.println("Employee Wage is:"+wage);
+			System.out.println("Employee Wage Per Month:"+wage);
 			break;
 		case 2:
 			System.out.println("Employee is Present Part Time");
-			System.out.println("Part Time  Employee Wage is:"+partTimeWage);
+			System.out.println("Part Time  Employee Wage Per Month:"+partTimeWage);
 		default:
 			System.out.println("Employee is absent");
 			
