14591e2ca790e91bfc4da10f356bedff8498b76f
Bharath-SR <bharath.s0941@gmail.com>
2022-09-14 07:53:34 +0530

    UC6:calculated Wage till Total working hours or days is reached

--- src/com/bridgeLabz/EmployeeWage.java
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -17,29 +17,47 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
-		Random random = new Random();
-		int attendance = random.nextInt(3); 
-		int wagePerHour = 20;
-		int fullDayHour = 8;
-		int perMonthDays = 20;
-		int partTime =4;
-		int wage = wagePerHour*fullDayHour*perMonthDays;
-		int partTimeWage= wagePerHour*partTime*perMonthDays;
+		final int WagesPerHours = 20;
+		final int totalWorkingHrs = 8;
+		int dailyWages = 0;
+		int partTimeWorkingHrs = 4;
+		 int totalWrkDays =20;
+		 int totalWages=0;
+		 final int maxWorkHrs = 100;
+		 int empHrs =0;
+		 int wrkdays =0;
+		 while (empHrs <= maxWorkHrs &&  wrkdays < totalWrkDays ){
+			 wrkdays++;
+			 System.out.println("Total Hours"+empHrs);
+		int random = (int)(Math.random()*3);
+		System.out.println(random);
 		
-
-		switch(attendance) {
+		switch (random) {
 		case 1:
-			System.out.println("Employee is Present");
-			System.out.println("Employee Wage Per Month:"+wage);
+			System.out.println("Employee is Present Full Time");
+			dailyWages = WagesPerHours*totalWorkingHrs;
+			totalWages = totalWages+dailyWages;
+			empHrs = empHrs+totalWorkingHrs;
+			System.out.println(totalWages);
 			break;
 		case 2:
 			System.out.println("Employee is Present Part Time");
-			System.out.println("Part Time  Employee Wage Per Month:"+partTimeWage);
-		default:
-			System.out.println("Employee is absent");
+			dailyWages = WagesPerHours*partTimeWorkingHrs;
+			System.out.println("PartTimeEmployee Daily Wages"+dailyWages);
+			totalWages = totalWages+dailyWages;
+			empHrs = empHrs+partTimeWorkingHrs;
+			System.out.println(totalWages);
+			break;
 			
+		case 0:
+			System.out.println("Employee is Absent");
 		}
-	}
 
+
+	}
+	
+		
+	
+
+}
 }
